60f6750af4566a95b5e21bf330a459232e326f2a
Jos Broers <jos@jobrodo.nl>
2022-09-08 22:35:42 +0200

    Rebuild composer scripts

--- web/wp-load.php
+++ web/wp-load.php
@@ -3,4 +3,5 @@
  * This file is here because some plugins like to do `require_once($_SERVER['DOCUMENT_ROOT'].'/wp-load.php');`
  * and our real wp-load is not found in the document root
  */
+
 require_once( __DIR__ . '/wp/wp-load.php' );
